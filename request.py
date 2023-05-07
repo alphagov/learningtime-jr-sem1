786001c432ab2422fefd798dbfc4b202ccc8009e
jonathan-ryding <jonathan.ryding@digital.cabinet-office.gov.uk>
2023-05-07 21:30:12 +0100

    Use psycopg2 library to connect and insert to DB so that codes and times will be saved.

--- request.py
+++ request.py
@@ -1,11 +1,41 @@
 import requests
 import time
+import psycopg2
 
 def check_endpoint(url):
     try:
         response = requests.get(url, timeout=5)
         response_time_ms = response.elapsed.total_seconds() * 1000
+
+        connector = psycopg2.connect(
+            dbname="postgres",
+            user="postgres",
+            password="mysecretpassword",
+            host="db",
+            port="5432"
+        )
         
+        with connector.cursor() as cursor:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS response_times (
+                    id SERIAL PRIMARY KEY,
+                    url VARCHAR(255) NOT NULL,
+                    response_code INTEGER NOT NULL,
+                    response_time_ms REAL NOT NULL,
+                    checked_at TIMESTAMP NOT NULL
+                );
+            """)
+            connector.commit()
+
+        with connector.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO response_times (url, response_code, response_time_ms, checked_at)
+                VALUES (%s, %s, %s, NOW());
+            """, (url, response.status_code, response_time_ms))
+            connector.commit()
+        
+        connector.close()
+
         if response.status_code == 200:
             print(f"The endpoint {url} is healthy. Response time: {response_time_ms:.0f} ms.")
         else:
