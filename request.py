944674b8656058d5237441abb3b31565ee119784
jonathan-ryding <jonathan.ryding@digital.cabinet-office.gov.uk>
2023-05-07 19:21:41 +0100

    Add server response time in milliseconds

--- request.py
+++ request.py
@@ -4,10 +4,13 @@ import time
 def check_endpoint(url):
     try:
         response = requests.get(url, timeout=5)
+        response_time_ms = response.elapsed.total_seconds() * 1000
+        
         if response.status_code == 200:
-            print(f"The endpoint {url} is healthy.")
+            print(f"The endpoint {url} is healthy. Response time: {response_time_ms:.0f} ms.")
         else:
-            print(f"The endpoint {url} returned status code: {response.status_code}.")
+            print(f"The endpoint {url} returned status code: {response.status_code}. Response time: {response_time_ms:.0f} ms.")
+
     except requests.exceptions.RequestException as exception:
         print(f"Request to {url} failed. Error: {str(exception)}")
 
