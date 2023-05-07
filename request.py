bcc279d4b14a77f341612f60a2ce19972f6f184e
jonathan-ryding <jonathan.ryding@digital.cabinet-office.gov.uk>
2023-05-07 22:21:41 +0100

    Implement failed DB connection error handling Added a try-except block so container doesn't shut down if db connection fails.

--- request.py
+++ request.py
@@ -6,8 +6,8 @@ def check_endpoint(url):
     try:
         response = requests.get(url, timeout=5)
         response_time_ms = response.elapsed.total_seconds() * 1000
-
-        connector = psycopg2.connect(
+        
+        conn = psycopg2.connect(
             dbname="postgres",
             user="postgres",
             password="mysecretpassword",
@@ -15,34 +15,33 @@ def check_endpoint(url):
             port="5432"
         )
         
-        with connector.cursor() as cursor:
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS response_times (
-                    id SERIAL PRIMARY KEY,
-                    url VARCHAR(255) NOT NULL,
-                    response_code INTEGER NOT NULL,
-                    response_time_ms REAL NOT NULL,
-                    checked_at TIMESTAMP NOT NULL
-                );
-            """)
-            connector.commit()
-
-        with connector.cursor() as cursor:
-            cursor.execute("""
-                INSERT INTO response_times (url, response_code, response_time_ms, checked_at)
-                VALUES (%s, %s, %s, NOW());
-            """, (url, response.status_code, response_time_ms))
-            connector.commit()
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS response_times (
+                        id SERIAL PRIMARY KEY,
+                        url VARCHAR(255) NOT NULL,
+                        response_code INTEGER NOT NULL,
+                        response_time_ms REAL NOT NULL,
+                        checked_at TIMESTAMP NOT NULL
+                    );
+                """)
+                conn.commit()
         
-        connector.close()
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO response_times (url, response_code, response_time_ms, checked_at)
+                    VALUES (%s, %s, %s, NOW());
+                """, (url, response.status_code, response_time_ms))
+                conn.commit()
 
-        if response.status_code == 200:
-            print(f"The endpoint {url} is healthy. Response time: {response_time_ms:.0f} ms.")
-        else:
-            print(f"The endpoint {url} returned status code: {response.status_code}. Response time: {response_time_ms:.0f} ms.")
-
-    except requests.exceptions.RequestException as exception:
-        print(f"Request to {url} failed. Error: {str(exception)}")
+        except Exception as e:
+            print(f"Database operation failed: {str(e)}")
+        finally:
+            conn.close()
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Request to {url} failed. Error: {str(e)}")
 
 url_to_check = 'https://www.python.org'
 
