c60254a5efe5321b63667f1444c8a2dda2ef95d4
jonathan-ryding <jonathan.ryding@digital.cabinet-office.gov.uk>
2023-05-16 20:12:00 +0100

    Add a route to query the database so that the data can be presented to the user using it

--- app/app.py
+++ app/app.py
@@ -3,9 +3,25 @@ import psycopg2
 
 app = Flask(__name__)
 
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="mysecretpassword",
+    host="db",
+    port="5432"
+)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/response-times')
+def get_response_times():
+    cur = conn.cursor()
+    cur.execute("SELECT checked_at, response_time_ms FROM response_times")
+    response_times = [{'checked_at': str(row[0]), 'response_time_ms': row[1]} for row in cur.fetchall()]
+    cur.close()
+    return jsonify(response_times)
 
 if __name__ == '__main__':
     app.run(debug=True)
