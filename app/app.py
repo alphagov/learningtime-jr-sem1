b1393762314ac0f919a285303a429c0447356267
jonathan-ryding <jonathan.ryding@digital.cabinet-office.gov.uk>
2023-05-16 20:09:51 +0100

    Add a Flask app that serves an index page using Docker

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
